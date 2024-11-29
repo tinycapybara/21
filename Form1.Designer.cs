ef49ab404d2f3f480f9797d3a9b52e4592e0828d
Dasha <Dasha@DESKTOP-I5KMMF9>
2024-11-30 00:15:05 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace _21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.btnStand = new System.Windows.Forms.Button();
+            this.lblBalance = new System.Windows.Forms.Label();
+            this.lblBet = new System.Windows.Forms.Label();
+            this.lblPlayerCards = new System.Windows.Forms.Label();
+            this.lblDealerCards = new System.Windows.Forms.Label();
+            this.txtBet = new System.Windows.Forms.TextBox();
+            this.lblPlayerScore = new System.Windows.Forms.Label();
+            this.lblDealerScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.BackColor = System.Drawing.Color.Pink;
+            this.btnStartGame.Font = new System.Drawing.Font("Verdana", 8.2F);
+            this.btnStartGame.Location = new System.Drawing.Point(590, 53);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(153, 37);
+            this.btnStartGame.TabIndex = 0;
+            this.btnStartGame.Text = "Начать игру";
+            this.btnStartGame.UseVisualStyleBackColor = false;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            // 
+            // btnHit
+            // 
+            this.btnHit.BackColor = System.Drawing.Color.Pink;
+            this.btnHit.Font = new System.Drawing.Font("Verdana", 8.2F);
+            this.btnHit.Location = new System.Drawing.Point(588, 187);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(155, 37);
+            this.btnHit.TabIndex = 1;
+            this.btnHit.Text = "Взять карту";
+            this.btnHit.UseVisualStyleBackColor = false;
+            this.btnHit.Click += new System.EventHandler(this.btnHit_Click);
+            // 
+            // btnStand
+            // 
+            this.btnStand.BackColor = System.Drawing.Color.Pink;
+            this.btnStand.Font = new System.Drawing.Font("Verdana", 8.2F);
+            this.btnStand.Location = new System.Drawing.Point(588, 267);
+            this.btnStand.Name = "btnStand";
+            this.btnStand.Size = new System.Drawing.Size(155, 37);
+            this.btnStand.TabIndex = 2;
+            this.btnStand.Text = "Завершить ход";
+            this.btnStand.UseVisualStyleBackColor = false;
+            this.btnStand.Click += new System.EventHandler(this.btnStand_Click);
+            // 
+            // lblBalance
+            // 
+            this.lblBalance.AutoSize = true;
+            this.lblBalance.Font = new System.Drawing.Font("Verdana", 10.2F);
+            this.lblBalance.Location = new System.Drawing.Point(16, 41);
+            this.lblBalance.Name = "lblBalance";
+            this.lblBalance.Size = new System.Drawing.Size(138, 20);
+            this.lblBalance.TabIndex = 4;
+            this.lblBalance.Text = "Баланс игрока";
+            // 
+            // lblBet
+            // 
+            this.lblBet.AutoSize = true;
+            this.lblBet.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblBet.Location = new System.Drawing.Point(17, 235);
+            this.lblBet.Name = "lblBet";
+            this.lblBet.Size = new System.Drawing.Size(150, 20);
+            this.lblBet.TabIndex = 5;
+            this.lblBet.Text = "Введите ставку ";
+            // 
+            // lblPlayerCards
+            // 
+            this.lblPlayerCards.AutoSize = true;
+            this.lblPlayerCards.Font = new System.Drawing.Font("Verdana", 10.2F);
+            this.lblPlayerCards.Location = new System.Drawing.Point(209, 319);
+            this.lblPlayerCards.Name = "lblPlayerCards";
+            this.lblPlayerCards.Size = new System.Drawing.Size(130, 20);
+            this.lblPlayerCards.TabIndex = 7;
+            this.lblPlayerCards.Text = "Карты игрока";
+            // 
+            // lblDealerCards
+            // 
+            this.lblDealerCards.AutoSize = true;
+            this.lblDealerCards.Font = new System.Drawing.Font("Verdana", 10.2F);
+            this.lblDealerCards.Location = new System.Drawing.Point(205, 23);
+            this.lblDealerCards.Name = "lblDealerCards";
+            this.lblDealerCards.Size = new System.Drawing.Size(134, 20);
+            this.lblDealerCards.TabIndex = 8;
+            this.lblDealerCards.Text = "Карты дилера";
+            // 
+            // txtBet
+            // 
+            this.txtBet.BackColor = System.Drawing.Color.LightPink;
+            this.txtBet.Font = new System.Drawing.Font("Verdana", 8.2F);
+            this.txtBet.Location = new System.Drawing.Point(16, 290);
+            this.txtBet.Name = "txtBet";
+            this.txtBet.Size = new System.Drawing.Size(151, 24);
+            this.txtBet.TabIndex = 9;
+            // 
+            // lblPlayerScore
+            // 
+            this.lblPlayerScore.AutoSize = true;
+            this.lblPlayerScore.Font = new System.Drawing.Font("Verdana", 10.2F);
+            this.lblPlayerScore.Location = new System.Drawing.Point(281, 378);
+            this.lblPlayerScore.Name = "lblPlayerScore";
+            this.lblPlayerScore.Size = new System.Drawing.Size(61, 20);
+            this.lblPlayerScore.TabIndex = 10;
+            this.lblPlayerScore.Text = "label1";
+            // 
+            // lblDealerScore
+            // 
+            this.lblDealerScore.AutoSize = true;
+            this.lblDealerScore.Font = new System.Drawing.Font("Verdana", 10.2F);
+            this.lblDealerScore.Location = new System.Drawing.Point(278, 80);
+            this.lblDealerScore.Name = "lblDealerScore";
+            this.lblDealerScore.Size = new System.Drawing.Size(61, 20);
+            this.lblDealerScore.TabIndex = 11;
+            this.lblDealerScore.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(778, 530);
+            this.Controls.Add(this.lblDealerScore);
+            this.Controls.Add(this.lblPlayerScore);
+            this.Controls.Add(this.txtBet);
+            this.Controls.Add(this.lblDealerCards);
+            this.Controls.Add(this.lblPlayerCards);
+            this.Controls.Add(this.lblBet);
+            this.Controls.Add(this.lblBalance);
+            this.Controls.Add(this.btnStand);
+            this.Controls.Add(this.btnHit);
+            this.Controls.Add(this.btnStartGame);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartGame;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.Button btnStand;
+        private System.Windows.Forms.Label lblBalance;
+        private System.Windows.Forms.Label lblBet;
+        private System.Windows.Forms.Label lblPlayerCards;
+        private System.Windows.Forms.Label lblDealerCards;
+        private System.Windows.Forms.TextBox txtBet;
+        private System.Windows.Forms.Label lblPlayerScore;
+        private System.Windows.Forms.Label lblDealerScore;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21
+{
+   
+    public partial class Form1 : Form
+    {
+        private Deck deck;
+        private List<Card> playerHand;
+        private List<Card> dealerHand;
+        private int playerBalance = 1000; // Начальный баланс
+        private int playerBet = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ResetGame(fullReset: true); // Полный сброс игры при запуске
+        }
+
+        private void ResetGame(bool fullReset = false)
+        {
+            deck = new Deck(); // Создаём новую колоду
+            playerHand = new List<Card>();
+            dealerHand = new List<Card>();
+            lblPlayerCards.Text = "Карты игрока: ";
+            lblDealerCards.Text = "Карты дилера: ";
+          //  lblResult.Text = "Результат: ";
+            lblPlayerScore.Text = "Очки игрока: 0";
+            lblDealerScore.Text = "Очки дилера: 0";
+            btnHit.Enabled = false;
+            btnStand.Enabled = false;
+
+            if (fullReset)
+            {
+                playerBalance = 1000; // Сбрасываем баланс при полном перезапуске
+                lblBalance.Text = $"Баланс: {playerBalance}";
+            }
+
+            // Очистка текстового поля для ставки
+            txtBet.Clear();
+            txtBet.Visible = true; // Показать текстбокс для новой игры
+            lblBet.Visible = true; // Показать поясняющий лейбл для текстбокса
+
+            // Скрываем остальные элементы управления, чтобы избежать визуального шума
+            lblPlayerCards.Visible = false;
+            lblDealerCards.Visible = false;
+            lblPlayerScore.Visible = false;
+            lblDealerScore.Visible = false;
+            btnHit.Visible = false;
+            btnStand.Visible = false;
+          //  lblResult.Visible = false;
+            btnStartGame.Visible = true;
+
+            btnStartGame.Enabled = true; // Включить кнопку старта игры
+        }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            // Если баланс равен 0, предлагать начать игру заново
+            if (playerBalance <= 0)
+            {
+                MessageBox.Show("Ваш баланс равен 0. Игра начинается заново.");
+                ResetGame(fullReset: true); // Полный сброс игры
+                return;
+            }
+
+            // Проверка корректности ставки
+            if (!int.TryParse(txtBet.Text, out playerBet) || playerBet <= 0 || playerBet > playerBalance)
+            {
+                MessageBox.Show("Введите корректную ставку.");
+                return;
+            }
+
+            // Снимаем ставку с баланса
+            playerBalance -= playerBet;
+            lblBalance.Text = $"Баланс: {playerBalance}";
+
+            // Скрыть текстбокс и лейбл после начала игры
+            txtBet.Visible = false;
+            lblBet.Visible = false;
+
+            // Показываем элементы для игрового процесса
+            lblPlayerCards.Visible = true;
+            lblDealerCards.Visible = true;
+            lblPlayerScore.Visible = true;
+            lblDealerScore.Visible = true;
+            btnHit.Visible = true;
+            btnStand.Visible = true;
+          //  lblResult.Visible = true;
+            btnStartGame.Visible = false;
+
+            // Очищаем руки игроков
+            playerHand.Clear();
+            dealerHand.Clear();
+
+            // Раздача карт
+            playerHand.Add(deck.DrawCard());
+            playerHand.Add(deck.DrawCard());
+
+            dealerHand.Add(deck.DrawCard());
+            dealerHand.Add(deck.DrawCard());
+
+            UpdateHands(false); // Обновление отображения (карты дилера скрыты)
+            btnHit.Enabled = true;
+            btnStand.Enabled = true;
+            btnStartGame.Enabled = false; // Отключить кнопку старта до конца раунда
+        }
+
+        private void btnHit_Click(object sender, EventArgs e)
+        {
+            playerHand.Add(deck.DrawCard());
+            UpdateHands(false); // Обновление отображения (карты дилера скрыты)
+
+            if (GetHandValue(playerHand) > 21)
+            {
+                EndGame("Перебор! Вы проиграли.");
+            }
+        }
+
+        private void btnStand_Click(object sender, EventArgs e)
+        {
+            while (GetHandValue(dealerHand) < 17)
+            {
+                dealerHand.Add(deck.DrawCard());
+            }
+
+            int playerScore = GetHandValue(playerHand);
+            int dealerScore = GetHandValue(dealerHand);
+
+            if (dealerScore > 21 || playerScore > dealerScore)
+            {
+                playerBalance += playerBet * 2;
+                EndGame("Вы выиграли!");
+            }
+            else if (playerScore < dealerScore)
+            {
+                EndGame("Вы проиграли.");
+            }
+            else
+            {
+                playerBalance += playerBet;
+                EndGame("Ничья.");
+            }
+        }
+
+        private void EndGame(string result)
+        {
+            MessageBox.Show(result); // Сообщение о результате игры
+            lblBalance.Text = $"Баланс: {playerBalance}";
+            btnHit.Enabled = false;
+            btnStand.Enabled = false;
+
+            UpdateHands(true); // Раскрываем карты дилера после завершения игры
+
+            // Проверка на окончание игры
+            if (deck.IsEmpty() || playerBalance <= 0)
+            {
+                if (playerBalance <= 0)
+                {
+                    MessageBox.Show("Ваш баланс равен 0. Игра начинается заново.");
+                    ResetGame(fullReset: true); // Полный сброс игры
+                }
+                else
+                {
+                    MessageBox.Show("Игра окончена. Начните заново.");
+                    ResetGame(false);
+                }
+            }
+
+            // Показать текстбокс и лейбл для новой ставки
+            txtBet.Visible = true;
+            lblBet.Visible = true;
+
+            // Включить кнопку старта игры, чтобы игрок мог начать новый раунд
+            btnStartGame.Visible = true; // Показываем кнопку Start
+            btnStartGame.Enabled = true; // Разрешаем кнопку Start
+        }
+
+        private void UpdateHands(bool revealDealer)
+        {
+            lblPlayerCards.Text = "Карты игрока: " + string.Join(", ", playerHand);
+            lblPlayerScore.Text = "Очки игрока: " + GetHandValue(playerHand);
+
+            if (revealDealer)
+            {
+                lblDealerCards.Text = "Карты дилера: " + string.Join(", ", dealerHand);
+                lblDealerScore.Text = "Очки дилера: " + GetHandValue(dealerHand);
+            }
+            else
+            {
+                lblDealerCards.Text = "Карты дилера: " + string.Join(", ", dealerHand.Take(1)) + ", [скрыто]";
+                lblDealerScore.Text = "Очки дилера: ?";
+            }
+        }
+
+        private int GetHandValue(List<Card> hand)
+        {
+            int value = hand.Sum(card => card.Value);
+            int aceCount = hand.Count(card => card.Rank == "Туз");
+
+            while (value > 21 && aceCount > 0)
+            {
+                value -= 10;
+                aceCount--;
+            }
+
+            return value;
+        }
+    }
+
+    public class Card
+    {
+        public string Rank { get; }
+        public string Suit { get; }
+        public int Value { get; }
+
+        public Card(string rank, string suit, int value)
+        {
+            Rank = rank;
+            Suit = suit;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Rank} {Suit}";
+        }
+    }
+
+    public class Deck
+    {
+        private List<Card> cards;
+        private Random random = new Random();
+
+        public Deck()
+        {
+            InitializeDeck(); // Инициализация колоды при создании
+        }
+
+        private void InitializeDeck()
+        {
+            cards = new List<Card>();
+
+            string[] suits = { "♠", "♥", "♦", "♣" };
+            string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз" };
+            int[] values = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11 };
+
+            foreach (var suit in suits)
+            {
+                for (int i = 0; i < ranks.Length; i++)
+                {
+                    cards.Add(new Card(ranks[i], suit, values[i]));
+                }
+            }
+        }
+
+        public Card DrawCard()
+        {
+            if (cards.Count == 0)
+            {
+                // Перезапуск колоды, если она пуста
+                InitializeDeck();
+                MessageBox.Show("Колода пуста. Колода перезапущена.");
+            }
+
+            // После перезапуска колоды или если колода не пуста, возвращаем карту
+            int index = random.Next(cards.Count);
+            Card card = cards[index];
+            cards.RemoveAt(index);
+            return card;
+        }
+
+        public bool IsEmpty() => cards.Count == 0;
+    }
+}
